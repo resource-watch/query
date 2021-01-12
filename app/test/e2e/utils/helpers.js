1a9a35c6bb37e447cc848441d17c63188f817ac9
Tiago Garcia <tiagojsag@gmail.com>
2021-01-12 13:31:29 +0100

    Update RW API integration MS to add fastly integration

--- app/test/e2e/utils/helpers.js
+++ app/test/e2e/utils/helpers.js
@@ -45,6 +45,12 @@ const createMockGetDataset = (id, anotherData = {}) => {
         });
 };
 
+const mockGetUserFromToken = (userProfile) => {
+    nock(process.env.CT_URL, { reqheaders: { authorization: 'Bearer abcd' } })
+        .get('/auth/user/me')
+        .reply(200, userProfile);
+};
+
 module.exports = {
-    createMockGetDataset
+    createMockGetDataset, mockGetUserFromToken
 };
